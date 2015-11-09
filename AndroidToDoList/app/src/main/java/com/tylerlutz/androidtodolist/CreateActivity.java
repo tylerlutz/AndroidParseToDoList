50b32d1a56d323c90c7278998b1472fdb93ef116
Tyler Lutz <tlutz.computers@gmail.com>
2015-11-09 16:23:28 -0500

    App completed.
    Application now can add, modify and delete tasks.

--- AndroidToDoList/app/src/main/java/com/tylerlutz/androidtodolist/CreateActivity.java
+++ AndroidToDoList/app/src/main/java/com/tylerlutz/androidtodolist/CreateActivity.java
@@ -8,9 +8,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseObject;
 
 public class CreateActivity extends AppCompatActivity {
 
+    private TextView txtName;
+    private TextView txtDescription;
+    private String name;
+    private String description;
+    private Button btnCreate;
     private Button btnBack;
 
     @Override
@@ -18,28 +27,37 @@ public class CreateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
-        btnBack = (Button) findViewById(R.id.btnBack);
-        btnBack.setOnClickListener(new View.OnClickListener() {
+        txtName = (TextView) findViewById(R.id.txtName);
+        txtDescription = (TextView) findViewById(R.id.txtDescription);
 
-            public void onClick(View arg0) {
+        btnCreate = (Button) findViewById(R.id.btnCreate);
+        btnCreate.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0){
+                name = txtName.getText().toString();
+                description = txtDescription.getText().toString();
 
-                Intent listActivity = new Intent(getApplicationContext(), ListActivity.class);
-                startActivity(listActivity);
+                if(name.equals("")&&description.equals("")){
+                    Toast.makeText(getApplicationContext(),
+                            "Please enter name and description",
+                            Toast.LENGTH_LONG).show();
+                }else {
+                    ParseObject task = new ParseObject("Task");
+                    task.put("name",name);
+                    task.put("description", description);
+                    task.saveInBackground();
 
+                    Intent listActivity = new Intent(getApplicationContext(),ListActivity.class);
+                    startActivity(listActivity);
+                }
             }
         });
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        btnBack = (Button) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0){
+                Intent listActivity = new Intent(getApplicationContext(), ListActivity.class);
+                startActivity(listActivity);
             }
         });
     }
-
 }
--- AndroidToDoList/app/src/main/java/com/tylerlutz/androidtodolist/UpdateActivity.java
+++ AndroidToDoList/app/src/main/java/com/tylerlutz/androidtodolist/UpdateActivity.java
@@ -6,11 +6,28 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.tylerlutz.androidtodolist.model.Task;
 
 public class UpdateActivity extends AppCompatActivity {
 
+    private TextView txtName;
+    private TextView txtDescription;
+    private Intent intent;
+    private Task task;
+    private String name;
+    private String description;
+    private Button btnUpdate;
+    private Button btnDelete;
     private Button btnBack;
 
     @Override
@@ -18,6 +35,88 @@ public class UpdateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
 
+        intent = getIntent();
+
+        task = new Task();
+        task.setTaskId(intent.getStringExtra("objectId"));
+
+        txtName = (TextView) findViewById(R.id.txtName);
+        txtDescription = (TextView) findViewById(R.id.txtDescription);
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Task");
+        query.getInBackground(task.getTaskId(), new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject task, ParseException e) {
+                if (e == null) {
+                    name = task.getString("name");
+                    description = task.getString("description");
+
+                    txtName.setText(name);
+                    txtDescription.setText(description);
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Something went wrong",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                name = txtName.getText().toString();
+                description = txtDescription.getText().toString();
+                if (name.equals("") && description.equals("")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Please enter name and description",
+                            Toast.LENGTH_LONG).show();
+
+                } else {
+                    ParseQuery<ParseObject> query = ParseQuery.getQuery("Task");
+                    query.getInBackground(task.getTaskId(), new GetCallback<ParseObject>() {
+                        public void done(ParseObject task, ParseException e) {
+                            if (e == null) {
+                                task.put("name", name);
+                                task.put("description", description);
+                                task.saveInBackground();
+
+                                Intent listActivity = new Intent(getApplicationContext(), ListActivity.class);
+                                startActivity(listActivity);
+                            } else {
+                                Toast.makeText(getApplicationContext(),
+                                        "Something went wrong",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("Task");
+                query.getInBackground(task.getTaskId(), new GetCallback<ParseObject>() {
+                    public void done(ParseObject task, ParseException e) {
+                        if (e == null) {
+
+                            task.deleteInBackground();
+
+                            Intent listActivity = new Intent(getApplicationContext(), ListActivity.class);
+                            startActivity(listActivity);
+
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    "Something went wrong",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+            }
+        });
         btnBack = (Button) findViewById(R.id.btnBack);
         btnBack.setOnClickListener(new View.OnClickListener() {
 
@@ -28,18 +127,5 @@ public class UpdateActivity extends AppCompatActivity {
 
             }
         });
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
